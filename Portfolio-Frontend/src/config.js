0c521e4724398f2e7a059a524c2e69a42f7488ee
SIDDHARTH SHEKHAR <75613480+siddharttth@users.noreply.github.com>
2024-10-22 21:34:08 +0530

    Update config.js

--- Portfolio-Frontend/src/config.js
+++ Portfolio-Frontend/src/config.js
@@ -1,10 +1,11 @@
 const API_URL = 'https://portfolio-backend-01.up.railway.app' || 'http://localhost:8000';
-
+// const API_URL = 'http://localhost:8000';
 export const endpoints = {
   education: `${API_URL}/api/education`,
   experience: `${API_URL}/api/experience`,
   projects: `${API_URL}/api/projects`,
-  skills: `${API_URL}/api/skills`
+  skills: `${API_URL}/api/skills`,
+  games: `${API_URL}/api/games`,
 };
 
 export default API_URL;
